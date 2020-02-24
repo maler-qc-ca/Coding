563e4a1017878ae09e98bf72a05fdbd357d31977
Moe Aler <moe.aler@hitachi-systems-security.com>
2020-02-23 23:25:21 -0500

    Starting Eloquent

--- Laravel/Laravel-master/app/Article.php
+++ Laravel/Laravel-master/app/Article.php
@@ -18,4 +18,9 @@ class Article extends Model
     {
         return route('articles.show', $this);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

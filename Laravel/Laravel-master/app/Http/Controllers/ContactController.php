8329b7d7022cbe63a2e22db0722968134b3ebc69
Moe Aler <moe.aler@hitachi-systems-security.com>
2020-08-12 16:42:27 -0400

    Email - HTML

--- Laravel/Laravel-master/app/Http/Controllers/ContactController.php
+++ Laravel/Laravel-master/app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMe;
 
 class ContactController extends Controller
 {
@@ -11,10 +12,13 @@ class ContactController extends Controller
         request()->validate(['email' => 'required|email']);
 
         $email = request('email');
-        Mail::raw('plain text message', function ($message) {
-            $message->to(request('email'))
-            ->subject('Subject');
-        });
+
+        // Mail::raw('plain text message', function ($message) {
+        //     $message->to(request('email'))
+        //     ->subject('Subject');
+        // });
+
+        Mail::to(request('email'))->send(new ContactMe('Laravel?'));
 
         return redirect('/articles/create')->with('message', 'Email Sent!');
     }
